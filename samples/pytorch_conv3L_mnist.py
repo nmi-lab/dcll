ee68475e8b3cbc4431bed270179ffacf42e044e8
Jacques KAISER <jkaiser@fzi.de>
2018-10-01 16:58:43 -0700

    single seed run but with and without first layer training

--- samples/pytorch_conv3L_mnist.py
+++ samples/pytorch_conv3L_mnist.py
@@ -36,18 +36,20 @@ def parse_args():
                         help='comment to name tensorboard files')
     parser.add_argument('--output', type=str, default='Results/',
                         help='folder name for the results')
+    parser.add_argument('--skip_first', type=bool, default=False, metavar='N', help='do not train first layer')
     return parser.parse_args()
 
 class ConvNetwork(torch.nn.Module):
     def __init__(self, im_dims, batch_size,
                  target_size, act,
                  loss, opt, opt_param, lc_ampl,
-                 alpha=[0.85, 0.9]
+                 alpha=[0.85, 0.9], skip_first=False
     ):
         super(ConvNetwork, self).__init__()
         # format: (out_channels, kernel_size, padding, pooling)
         convs = [ (16, 7, 3, 2), (24, 7, 3, 2), (32, 7, 3, 1) ]
         self.batch_size = batch_size
+        self.skip_first = skip_first
 
         def make_conv(inp, conf):
             layer = Conv2dDCLLlayer(in_channels = inp[0], out_channels = conf[0],
@@ -67,6 +69,10 @@ class ConvNetwork(torch.nn.Module):
         self.layer2, n = make_conv(n, convs[1])
         self.layer3, n = make_conv(n, convs[2])
 
+        if self.skip_first:
+            # scale up the first layer weights if we don't train it
+            self.layer1.i2h.weight.data.mul_(100.)
+
         self.dcll_slices = []
         for layer, name in zip([self.layer1, self.layer2, self.layer3],
                                ['conv1', 'conv2', 'conv3']):
@@ -85,8 +91,12 @@ class ConvNetwork(torch.nn.Module):
 
     def train(self, x, labels):
         spikes = x
-        for sl in self.dcll_slices:
-            spikes, _, pv = sl.train(spikes, labels)
+        for i, sl in enumerate(self.dcll_slices):
+            if self.skip_first and i==0:
+                # if skip first is on we don't train the first layer
+                spikes, _, pv = sl.forward(spikes)
+            else:
+                spikes, _, pv = sl.train(spikes, labels)
 
     def test(self, x):
         spikes = x
@@ -126,7 +136,8 @@ if __name__ == "__main__":
 
     net = ConvNetwork(im_dims, args.batch_size, target_size,
                       act=torch.nn.Sigmoid(), alpha=[args.alpha, args.alphas],
-                      loss=loss, opt=opt, opt_param=opt_param, lc_ampl=args.lc_ampl
+                      loss=loss, opt=opt, opt_param=opt_param, lc_ampl=args.lc_ampl,
+                      skip_first=args.skip_first
     )
 
     from tensorboardX import SummaryWriter
@@ -136,7 +147,7 @@ if __name__ == "__main__":
     if not args.no_save:
         d = mksavedir(pre=args.output)
         annotate(d, text = log_dir, filename= 'log_filename')
-        annotate(d, text = str(args), filename= 'args')
+        annotate(d, text = str(args), filename= 'args.pkl')
         save_source(d)
 
     n_tests_total = np.ceil(float(args.n_epochs)/args.n_test_interval).astype(int)
@@ -185,7 +196,7 @@ if __name__ == "__main__":
                 }
                 with open(d+'/parameters_{}.pkl'.format(epoch), 'wb') as f:
                     pickle.dump(parameter_dict, f)
-            print("Epoch {} \t Accuracy {}".format(epoch, acc_test[epoch//args.n_test_interval, 0, -1]))
+            print("Epoch {} \t Accuracy {}".format(epoch, acc_test[epoch//args.n_test_interval, 0, :]))
 
 
 
