2907ae6b0199d230ef2194dc08b491c7ee33d92f
Jacques KAISER <jkaiser@fzi.de>
2018-10-01 14:32:20 -0700

    bash loop compatibility

--- samples/run_mnist_experiments.sh
+++ samples/run_mnist_experiments.sh
@@ -1,15 +1,15 @@
 n_test_interval=5
 n_epoch=100
 n_runs=10
-seed=0
 cwd=`dirname "$0"`
 program=${cwd}/pytorch_conv3L_mnist.py
 results="Paper_results/"
+seed=0
 
-tput setaf 2; echo "Results will be saved in ${results}"
-for i in {1..${n_runs}}
+tput setaf 2; echo "Results will be saved in ${results}"; tput setaf 9
+while [ ${seed} -lt ${n_runs} ]
 do
-    tput setaf 2; echo "Running ${program} with seed ${seed}"
+    tput setaf 2; echo "Running ${program} with seed ${seed}"; tput setaf 9
     ${program} --seed ${seed} --n_epochs ${n_epoch} --n_test_interval ${n_test_interval} --output ${results}
-    ((seed++))
+    seed=$(( ${seed} + 1 ))
 done
